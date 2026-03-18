9b2421fccf8dca65caeb24af2876a800774ceb66
iStormathy <stormageddon473@yahoo.com>
2026-03-18 19:01:00 -0400

    Changed the name of the variable "recipe" to "originalRecipe" in order to clarify its purpose. Corrected the variable types of "originalRecipe" and "request" to int, as double was unneccessary

--- Ingredient_Adjustment_Project/Ingredient_Adjustment_Project.cpp
+++ Ingredient_Adjustment_Project/Ingredient_Adjustment_Project.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main()
 {
-	double recipe = 48, request; // Number of cookies for the original recipe.
+	int originalRecipe = 48, request; // Number of cookies for the original recipe.
 	cout << "How many cookies would you like to make?" << endl;
 	cin >> request;
-	double sugar = request * (1.5 / recipe); // adjusts the proportions according to specific requests
-	double butter = request * (1.0 / recipe);
-	double flour = request * (2.75 / recipe);
+	double sugar = request * (1.5 / originalRecipe); // adjusts the proportions according to specific requests
+	double butter = request * (1.0 / originalRecipe);
+	double flour = request * (2.75 / originalRecipe);
 	cout << "To make " << request << " cookies, you will need the following ingredients:" << endl;
 	cout << fixed << setprecision(2) << sugar << " cups of sugar" << endl;
 	cout << fixed << setprecision(2) << butter << " cups of butter" << endl;
